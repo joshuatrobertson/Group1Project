eb5d7402197bd7c61c0c3e9d302381724f4bbba8
joshuatrobertson <joshuatrobertson@protonmail.com>
2021-02-23 15:20:39 +0000

    Updated site to use the Chart.js framework

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {ChartsModule} from 'ng2-charts';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BarChartComponent} from './bar-chart/bar-chart.component';
+import {DoughnutChartComponent} from './doughnut-chart/doughnut-chart.component';
+import {RadarChartComponent} from './radar-chart/radar-chart.component';
+import {PieChartComponent} from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
