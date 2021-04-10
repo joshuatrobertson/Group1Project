2a00d089f96ea2c193690aea269d7678e43e1695
Efan <efanhaynes@gmail.com>
2021-04-10 01:22:43 +0100

    testing adding entires to mongo

--- site/server/routes/api.js
+++ site/server/routes/api.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Player = require('../../models/player');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.send('api works');
+  res.send('Testing123');
 });
 
+router.get('/test', (req, res) => {
+  res.send('Testing456');
+});
+
+router.post('/testdata', (req, res) => {
+  const player = new Player({
+    username: req.body.username
+    });
+  player.save()
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => {
+    res.json({ message: err });
+  });
+});
 module.exports = router;
