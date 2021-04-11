88a8107204f762379ec964750eaf9d49b62b7686
Efan <efanhaynes@gmail.com>
2021-04-12 00:14:18 +0100

    Adding basic API functionality

--- site/server/routes/api.js
+++ site/server/routes/api.js
@@ -1,26 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const Player = require('../../models/player');
+const data = require('./data.json')
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  res.send('Testing123');
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(data));
 });
 
-router.get('/test', (req, res) => {
-  res.send('Testing456');
-});
-
-router.post('/testdata', (req, res) => {
+//Add a player to the database
+router.post('/', async (req, res) => {
   const player = new Player({
-    username: req.body.username
+    username: req.body.username,
+    birthday: req.body.birthday
     });
-  player.save()
-  .then(data => {
-    res.json(data);
-  })
-  .catch(err => {
+  try{
+    const savedPlayer = await player.save()
+    res.json(savedPlayer);
+  } catch (err) {
     res.json({ message: err });
-  });
+  }
 });
+
+//Return all players stored in the database
+router.get('/getplayers', async (req, res) => {
+  try {
+    const players = await Player.find();
+    res.json(players);
+  } catch (err) {
+    res.json({message : err});
+  }
+});
+
+//Get a specific player
+router.get('/:playerId', async (req, res) => {
+  try {
+    const player = await Player.findById(req.params.playerId)
+    res.json(player);
+  } catch (err) {
+    res.json({message : err});
+  }
+});
+
+
+
+
 module.exports = router;
