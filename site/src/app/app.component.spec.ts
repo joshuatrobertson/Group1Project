68a82c038df28221a4be48ee19333dd34786d7a7
Josh <joshuatrobertson@protonmail.com>
2021-04-29 10:30:51 +0100

    testing app.component

--- site/src/app/app.component.spec.ts
+++ site/src/app/app.component.spec.ts
@@ -10,12 +10,14 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  // Make sure the application is created
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
+  // Set the title as card-game-new
   it(`should have as title 'card-game-new'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -28,4 +30,23 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('card-game-new app is running!');
   });
+
+  // Make sure the application is created
+  it('begin animation variables should be set to false', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.beginAnimations).toBeTruthy();
+    expect(app.beginAnimations2).toBeTruthy();
+    expect(app.beginAnimations3).toBeTruthy();
+  });
+
+  // Make sure the application is created
+  it('begin sound variables should be set to true', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.beginSound).toBeFalsy();
+    expect(app.beginSound2).toBeFalsy();
+    expect(app.beginSound3).toBeFalsy();
+  });
+
 });
