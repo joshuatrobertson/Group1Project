3394c73b62976252718360bcf2fcc70b529a0eb4
Josh <joshuatrobertson@protonmail.com>
2021-05-01 18:10:06 +0100

    formatting

--- site/src/app/bar-chart/bar-chart.component.ts
+++ site/src/app/bar-chart/bar-chart.component.ts
@@ -27,19 +27,19 @@ export class BarChartComponent implements OnInit {
 
   constructor(private dataService: DataService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.retrieveData();
   }
 
-  clickButtonHome= function () {
+  clickButtonHome = function(): void {
     this.router.navigateByUrl('/');
   };
 
-  retrieveData() {
+  retrieveData(): void {
     this.dataService.getPlays().subscribe(
         data => {
           this.stats = data;
-          let avgTimes: Array<number> = [0,0];
+          let avgTimes: Array<number> = [0, 0];
           let length = this.stats.length;
           let j = 0;
           for (let i = 0; i < length; i++) {
