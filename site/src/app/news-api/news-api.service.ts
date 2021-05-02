33084485294b3c5374f022c5196cf1b84782132f
Josh <joshuatrobertson@protonmail.com>
2021-05-02 16:45:12 +0100

    api key added

--- site/src/app/news-api/news-api.service.ts
+++ site/src/app/news-api/news-api.service.ts
@@ -6,9 +6,10 @@ import { HttpClient  } from '@angular/common/http';
 })
 export class NewsApiService {
 
-  apiKey = '1a566dd2a11c4173b47ada5f97e6940a';
+  //apiKey = '1a566dd2a11c4173b47ada5f97e6940a';
   // backup key
  // apiKey = '28cabbff7add46febde4eb4e84536f71';
+  apiKey = 'dc1f675523b34b66aea3c2bbd19287fb';
 
   newsSources: Array<string> = ['entertainment-weekly', 'mtv-news', 'mtv-news-uk', 'next-big-future', 'reddit-r-all', 'vice-news'];
 
